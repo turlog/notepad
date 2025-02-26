847f97bd5186cb653342053d6481a27ed17058c1
Łukasz Zosiak <lukasz.zosiak@gmail.com>
2025-02-26 06:19:02 +0100

    Python 3.13+ __replace__ protocol

--- copy_replace.py
+++ copy_replace.py
@@ -25,3 +25,26 @@ print(
         date=replace(order.date, year=2024),
     )
 )
+
+
+class Invoice:
+
+    def __init__(self, order):
+        self.total = order.total
+
+    def __replace__(self, order=None, total=None):
+        if order is not None:
+            return Invoice(order)
+        if total is not None:
+            invoice = Invoice.__new__(Invoice)
+            invoice.total = total
+            return invoice
+
+    def __repr__(self):
+        return f"Invoice(id={id(self)}, total={self.total})"
+
+
+print(invoice := Invoice(order))
+invoice.total = Amount(100, "PLN")
+print(invoice)
+print(replace(invoice, order=order))
